cf1012b69cef839a653dc1fe94ef1383b308dc95
azadeh.samadi <samadi.azadeh@gmail.com>
2023-07-06 16:04:58 +0200

    disply calender menu

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,31 @@
 package com.example;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+   /*
+   this method will print
+   console-based calculator text to the console
+    */
+
+    // this is the main method not allow to change it
+    public static void main(String[] args) {
+
+        System.out.println("Following Operations Are Supported : \n" +
+                "1. Addition (+) \n" +
+                "2. Subtraction (-) \n" +
+                "3. Multiplication (*) \n" +
+                "4. Division (/) \n");
+
+        System.out.println("Select a valid operation ");
+        Scanner scanner=new Scanner(System.in);
+        int operationNumber = scanner.nextInt();
+
+        // if else or switch
+
+
     }
 }
